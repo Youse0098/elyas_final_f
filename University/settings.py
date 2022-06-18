b74bf27831fee3ef82c5af93ac1c4ca97da303a3
Youse0098 <93992241+Youse0098@users.noreply.github.com>
2022-06-18 22:01:13 +0430

    Update settings.py

--- University/settings.py
+++ University/settings.py
@@ -98,8 +98,6 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-# Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
