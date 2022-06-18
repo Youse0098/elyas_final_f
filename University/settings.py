f187bee4b5a9e0bad904a21689c051a29ad1f943
Youse0098 <93992241+Youse0098@users.noreply.github.com>
2022-06-18 21:58:39 +0430

    Update settings.py

--- University/settings.py
+++ University/settings.py
@@ -98,15 +98,8 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-# DATABASES = {
-#     'default': {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": "sqlite3",
-#     }
-# }
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
